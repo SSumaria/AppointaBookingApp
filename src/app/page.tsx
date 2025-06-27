f1b81a0589b803938e89399ecdd6da19a7e8d067
Shyam Shah <shyamsumaria96@gmail.com>
2025-06-27 09:58:11 +0000

    added command-centre2 image

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -196,7 +196,7 @@ export default function LandingPage() {
           <div className="container mx-auto px-4 grid md:grid-cols-2 gap-12 items-center">
              <AnimatedSection initialClasses="opacity-0 -translate-x-12" delay={200}>
                <Image 
-                 src="/images/command-centre1.png" 
+                 src="/images/command-centre2.png" 
                  alt="Appointa dashboard and calendar view" 
                  width={600} 
                  height={400} 
